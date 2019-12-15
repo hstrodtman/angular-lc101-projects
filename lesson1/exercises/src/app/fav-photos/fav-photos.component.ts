221244ba55fa2d079499f81e85a0ad2f97111141
hstrodtman <heatherstrodtman@gmail.com>
2019-12-15 11:02:01 -0600

    adding favorite links and page title components and modifying css

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.pinimg.com/originals/30/9e/c6/309ec688553bd7e06beb111904c9a71a.jpg';
+  image3 = 'http://earthporm.com/wp-content/uploads/2015/05/bulldog-puppy-cute-dog-photography-36__605.jpg';
 
   constructor() { }
 
